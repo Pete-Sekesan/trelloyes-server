19bf86a98ffa197dd66bcc8ff6ccd7cecf71572c
Peter Sekesan <psekesan@gmail.com>
2021-02-04 13:30:40 -0500

    validationBearer added

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   transports: [new winston.transports.File({ filename: "info.log" })],
 });
-
 if (NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
@@ -22,6 +21,21 @@ if (NODE_ENV !== "production") {
     })
   );
 }
+
+const cards = [
+  {
+    id: 1,
+    title: "Task One",
+    content: "This is card one",
+  },
+];
+const lists = [
+  {
+    id: 1,
+    header: "List One",
+    cardIds: [1],
+  },
+];
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
@@ -42,6 +56,17 @@ app.use(function errorHandler(error, req, res, next) {
     response = { message: error.message, error };
   }
   res.status(500).json(response);
+});
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  // move to the next middleware
+  next();
 });
 
 module.exports = app;
